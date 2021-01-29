ec3dfcf032ed72196a3c42d80a1d61fa9290fd8a
Vortezz <61989315+Vortezz@users.noreply.github.com>
2021-01-29 22:55:21 +0100

    Update ping.js

--- commandsDirectory/ping.js
+++ commandsDirectory/ping.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-  //The code for your command take place here
+  //Set your code here
   message.channel.send(`**🏓 The latency is ${Date.now() - message.createdTimestamp} ms.**`);
 }
-//You can change the name of the command here
+//Change the command name !
 module.exports.help = {
   name: "ping",
   description: "Show the latency of the BOT"
