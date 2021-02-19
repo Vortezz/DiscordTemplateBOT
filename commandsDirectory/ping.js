b2c5663e4534eaad2a07af99c330be356d8d2e61
Vortezz <61989315+Vortezz@users.noreply.github.com>
2021-02-19 21:57:57 +0100

    Update ping.js
    Pimp the ping command !

--- commandsDirectory/ping.js
+++ commandsDirectory/ping.js
@@ -1,10 +1,19 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-  //Set your code here
-  message.channel.send(`**🏓 The latency is ${Date.now() - message.createdTimestamp} ms.**`);
-}
-//Change the command name !
+    /*Place your code here !*/
+    const pinging = new Discord.MessageEmbed()
+        	.setTitle(`Pinging...`)
+        	.setColor('RANDOM')
+        	.setTimestamp();
+    message.channel.send(pinging).then((msg)=>{
+    const ping = new Discord.MessageEmbed()
+        	.setTitle(`:ping_pong: **Latency : ${msg.createdTimestamp - message.createdTimestamp}ms**`)
+        	.setColor('RANDOM')
+        	.setTimestamp();
+    msg.edit(ping);
+  })
+/*Change the command name !*/
 module.exports.help = {
   name: "ping",
   description: "Show the latency of the BOT"
