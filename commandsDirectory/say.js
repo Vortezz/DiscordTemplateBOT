c9dc4880b9de9faee7de079e5ebea59b2a794437
Vortezz <61989315+Vortezz@users.noreply.github.com>
2021-03-14 22:19:20 +0100

    Fixed say.js

--- commandsDirectory/say.js
+++ commandsDirectory/say.js
@@ -1,23 +1,21 @@
 const Discord = require('discord.js')
-const emojis = require('../config/emojis.json')
-const color = require('../config/colors.json')
 
 module.exports.run = async (client, message, args) => {
     if (!args[0]) {
         const error = new Discord.MessageEmbed()
-        .setTitle(`${emojis.error} Erreur`)
-        .setDescription("Tu n'as pas mis de texte à me faire dire...")
-        .setColor(color.RED)
+        .setTitle(`:x: Error`)
+        .setDescription("You haven't say any text...")
+        .setColor('RED')
         .setTimestamp();
         message.channel.send(error)
         return
     }
     text = message.content.split(' ').slice(1).join(' ');
-    text = text.replace('config.token', 'On ne va pas te donner le token quand même...');
+    text = text.replace('config.token', 'We will not give the token :)');
     const say = new Discord.MessageEmbed()
-        .setTitle(`Texte de ${message.author.username}`)
+        .setTitle(`Text of ${message.author.username}`)
         .setDescription(text)
-        .setColor(color.GREEN)
+        .setColor('GREEN')
         .setTimestamp();
         message.channel.send(say)
 }
